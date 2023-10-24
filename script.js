c4a7ef3b49edea91c8005519db1b52bda38ce0fb
StylePoints420 <stylepoints420@gmail.com>
2023-10-24 22:24:31 +0800

    Add a function to know the game result

--- script.js
+++ script.js
@@ -15,12 +15,7 @@ function getComputerChoice() {
 
 }
 
-function playRound (playerSelection, computerSelection) {
-    let tieMessage = `It's a tie! ${playerSelection.toUpperCase()} vs ${computerSelection} is a draw!`;
-    let loseMessage  = `You Lost to the computer! ${computerSelection} beats ${playerSelection.toUpperCase()}`;
-    let winMessage = `You won against the computer! ${playerSelection.toUpperCase()} beats ${computerSelection}`;
-    // let cc = computerSelection.toUpperCase();
-    // console.log(cc);
+function playRound (playerSelection, computerSelection, tieMessage, loseMessage, winMessage) {
     if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
         return tieMessage;
     }
@@ -34,8 +29,47 @@ function playRound (playerSelection, computerSelection) {
         || (computerSelection.toUpperCase() === "SCISSORS" && playerSelection.toUpperCase() === "PAPER")) {
         return loseMessage;
     }
+    else {
+        return;
+    }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let message = "Enter your choice: Rock, Paper or Scissors";
+    let playerSelection;
+    const computerSelection = getComputerChoice();
+    let counter;
+    let playerWin = 0, computerWin = 0, tieMark = 0;
+        for(counter = 1; counter <= 5; counter++) {
+            playerSelection = prompt(message);
+            let tieMessage = `It's a tie! ${playerSelection} vs ${computerSelection} is a draw!`;
+            let loseMessage  = `You Lost to the computer! ${computerSelection} beats ${playerSelection}`;
+            let winMessage = `You won against the computer! ${playerSelection} beats ${computerSelection}`;
+            playRound(playerSelection, computerSelection, tieMessage, loseMessage, winMessage);
+            if (playRound(playerSelection, computerSelection, tieMessage, loseMessage, winMessage) === winMessage) {
+                alert(winMessage);
+                playerWin += 1;
+            }
+            else if (playRound(playerSelection, computerSelection, tieMessage, loseMessage, winMessage) === loseMessage) {
+                alert(loseMessage);
+                computerWin += 1;
+            }
+            else if (playRound(playerSelection, computerSelection, tieMessage, loseMessage, winMessage) === tieMessage) {
+                alert(tieMessage);
+            }
+            else {
+                alert("You did not enter the correct choices, you will not earn a score for this round!");
+            }
+        }
+
+    if (playerWin > computerWin) {
+        alert(`Player wins! With a total score of ${playerWin} and the computer with a score of ${computerWin}`);
+    }
+    else if (computerWin > playerWin) {
+        alert(`Computer wins! <br> With a total score of ${computerWin} and the player with a score of ${playerWin}`);
+    }
+    else if (computerWin === playerWin) {
+        alert (`It's a tie! Both player and computer has the total score of ${playerWin}`);
+    }
+}
+game();
